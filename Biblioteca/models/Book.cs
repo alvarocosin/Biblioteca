0cf53c507206ef79c6c2015cbcf2b6887e318fc8
Álvaro Cosín <cosin.alvaro@gmail.com>
2023-01-03 12:40:45 +0100

    Book class created

---
+++ Biblioteca/models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.models
+{
+    public class Book
+    {
+        public Book(string title, string author, string bookshelf)
+        {
+            Title = title;
+            Author = author;
+            Bookshelf = bookshelf;
+        }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Bookshelf { get; set; }
+    }
+
+}
